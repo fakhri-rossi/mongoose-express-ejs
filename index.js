2c7abb1b7d72c83721563dbef1d2c336b9bbb58b
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-18 19:40:11 +0700

    Fixed bug: stored garment id to product.garment when a product being added to a garment

--- index.js
+++ index.js
@@ -76,7 +76,10 @@ app.post('/garments/:garment_id/products', wrapAsync(async (req, res) => {
     const { garment_id } = req.params;
     const garment = await Garment.findById(garment_id);
     const product = new Product(req.body);
+    
     garment.products.push(product);
+    product.garment = garment;
+
     await garment.save();
     await product.save();
     res.redirect(`/garments/${garment_id}`);
